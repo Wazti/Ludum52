17d45af53347829f6b209ff55be7a9237f352111
Wazti <iakudrya@edu.hse.ru>
2023-01-08 16:20:47 +0300

    fix

---
+++ Assets/CodeBase/Buildings/BuildingFilling.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace CodeBase.Buildings
+{
+    public class BuildingFilling : MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer _renderer;
+        
+        private void Awake()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Assets/CodeBase/Camera/ParallaxLayer.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace CodeBase.Camera
+{
+    [ExecuteInEditMode]
+    public class ParallaxLayer : MonoBehaviour
+    {
+        public float parallaxFactor;
+
+        public void Move(float delta)
+        {
+            Vector3 newPos = transform.localPosition;
+            newPos.x -= delta * parallaxFactor;
+            transform.localPosition = newPos;
+        }
+    }
+
+    [ExecuteInEditMode]
+    public class ParallaxCamera : MonoBehaviour
+    {
+        public delegate void ParallaxCameraDelegate(float deltaMovement);
+
+        public ParallaxCameraDelegate onCameraTranslate;
+        private float oldPosition;
+
+        void Start()
+        {
+            oldPosition = transform.position.x;
+        }
+
+        void Update()
+        {
+            if (transform.position.x != oldPosition)
+            {
+                if (onCameraTranslate != null)
+                {
+                    float delta = oldPosition - transform.position.x;
+                    onCameraTranslate(delta);
+                }
+
+                oldPosition = transform.position.x;
+            }
+        }
+    }
+}
